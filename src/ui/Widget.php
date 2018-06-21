7bde8e91054e4b29811572c762cd9ed31fb76974
Nick Panteleeff <drnix@chess-online.com>
2018-06-21 18:55:49 +0300

    Fix culture initialization

--- src/ui/Widget.php
+++ src/ui/Widget.php
@@ -150,7 +150,12 @@ abstract class Widget extends YiiWidget implements Serializable
         }
 
         $lang = Yii::$app->language;
-        $view->registerJs("kendo.culture('{$lang}');", View::POS_READY, "KendoUICultureInitialize");
+        $view->registerJs(<<<JS
+if (window.kendo) {
+    kendo.culture('{$lang}');    
+}
+JS
+        , View::POS_READY, "KendoUICultureInitialize");
     }
 
     private function escapeSelector($value)
